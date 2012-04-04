dd5ae414f95f439c9658f5d3d5de0b60f5ff4eac
Robert Hollencamp <rhollencamp@gmail.com>
2012-04-04 17:45:32 -0400

    initial checkin

---
+++ src/main/java/com/thebinaryidiot/springnoxml/WebAppConfig.java
@@ -0,0 +1,47 @@
+/*
+ *   Copyright 2012 Robert Hollencamp
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.thebinaryidiot.springnoxml;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Configuration for Spring Web
+ *
+ * @author robert.hollencamp
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.thebinaryidiot.springnoxml.controller")
+class WebAppConfig
+{
+	/**
+	 * Configure view resolver to look in /WEB-INF/views/ for .jspx files
+	 *
+	 * @return
+	 */
+	@Bean
+	public InternalResourceViewResolver configureInternalResourceViewResolver()
+	{
+		final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jspx");
+		return resolver;
+	}
+}
